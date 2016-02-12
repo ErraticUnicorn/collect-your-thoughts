aa21f8f7a38a6bdf19240bbc377e92fd3018d707
Kevin Henke <henkekm@gmail.com>
2016-02-12 08:22:38 -0500

    Begins moving movement control to main camera

---
+++ Assets/Scripts/mouseController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseController : MonoBehaviour {
+	public Vector3 conciousPosition;
+	Camera thisCamera;
+
+	void Start() {
+		thisCamera = GetComponent<Camera>();
+	}
+
+	void Update() {
+		Ray ray = thisCamera.ScreenPointToRay(Input.mousePosition);
+		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+		conciousPosition = ray.origin;
+	}
+}
--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class move : MonoBehaviour {
-	
+	private mouseController mouse_controller;
 	private GameObject child;
 	// Use this for initialization
 	void Start () {
+		Camera camera = Camera.main;
+		mouse_controller = camera.GetComponent<mouseController>();
 		UnityEngine.Cursor.visible= false;
 		GameObject conscious = this.gameObject;
 		SpriteRenderer marker = conscious.GetComponentInChildren<SpriteRenderer> ();
@@ -16,6 +18,8 @@ public class move : MonoBehaviour {
 	void Update () {
 
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		// update position assignment from mouse_controller
+		Debug.Log (mouse_controller.conciousPosition.x);
 		this.transform.position = new Vector3(ray.origin.x, ray.origin.y, ray.origin.z);
 
 		Debug.DrawRay(Camera.main.gameObject.transform.position, ray.direction, Color.white);
